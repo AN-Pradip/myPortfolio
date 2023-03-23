6b108a866148137ea5ccd6785cd061c35ca7eed9
AN-Pradip <ppradip.an@gmail.com>
2023-03-23 13:57:08 +0100

    Adding contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,9 +3,14 @@ import React from "react";
 function Contact(){
 
     return(
-        <main> 
-            hoho
-        </main>
+        <section className="Contact"> 
+            <h2> Contactez moi !</h2>
+            <ul>
+                <li> <span> E-mail : </span> <a href="mailto:ppradip.an@gmail.com"> ppradip.an@gmail.com </a>  </li>
+                <li> <span> Téléphone : </span> <a href="tel:+33651017102"> 06 51 01 71 02 </a> </li>
+                <li> <span> LinkedIn : </span> <a href="https://www.linkedin.com/in/pradip-ananda/"> https://www.linkedin.com/in/pradip-ananda/</a></li>
+            </ul>
+        </section>
     )
 }
 
